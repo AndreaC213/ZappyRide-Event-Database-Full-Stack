fd238dc1af0e270cc9cd8297216ec191e4a46e75
Andrea Chang <andrea.chang213@gmail.com>
2020-06-17 10:46:25 -0400

    deploy debug

--- front-end_deveploment/src/App.js
+++ front-end_deveploment/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import './App.css'
 import EventList from './components/event-list.component/event-list.component'
 import EventForm from './components/event-form.component/event-form.component'
-import List from './components/List.component/List.component'
 
 class App extends Component {
 	render() {
